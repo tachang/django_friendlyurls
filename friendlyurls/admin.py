627d79ae4950338c8a5a0d75bae244c9c0374d4a
Jeff Tchang <Jeff.Tchang@gmail.com>
2013-02-04 04:18:28 +0000

    Allow searching of vanity urls

--- friendlyurls/admin.py
+++ friendlyurls/admin.py
@@ -3,5 +3,6 @@ from friendlyurls.models import *
 
 class UrlMappingAdmin(admin.ModelAdmin):
   list_display = ('friendly_path', 'resolved_url', 'content_type', 'object')
+  search_fields = ('friendly_path','content_type__name')
 admin.site.register(UrlMapping, UrlMappingAdmin)
 
